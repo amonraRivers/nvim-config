655b839c9ebb1c6cfdc92a182eb974faaf7cf2b0
Ramon Rios <ramosquito5@gmail.com>
2023-09-08 13:09:38 -0600

    modifying nvim to show hidden files and ignore .git

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps',function()
-	builtin.grep_string({search=vim.fn.input("Grep> ")});
-end)
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        find_command = { 'rg', '--files', '--glob', '!.git', '--hidden' },
+    });
+end, {})
+vim.keymap.set('n', '<C-p>', function()
+    builtin.git_files({});
+end, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep> ") });
+end, {})
--- lua/amonra/packer.lua
+++ lua/amonra/packer.lua
@@ -4,45 +4,45 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use 'github/copilot.vim'
+    use 'github/copilot.vim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use 'mbbill/undotree'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'L3MON4D3/LuaSnip'
-	use 'jose-elias-alvarez/null-ls.nvim'
-	use 'MunifTanjim/prettier.nvim'
-	use 'simrat39/rust-tools.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use 'mbbill/undotree'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'L3MON4D3/LuaSnip'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'MunifTanjim/prettier.nvim'
+    use 'simrat39/rust-tools.nvim'
 
-	-- Debugging
-	use 'nvim-lua/plenary.nvim'
-	use 'mfussenegger/nvim-dap'
-	use 'alx741/vim-rustfmt'
-	use 'windwp/nvim-autopairs'
-	use 'nvim-treesitter/nvim-treesitter'
-	use {
-		'windwp/nvim-ts-autotag',
-	}
-	use 'leafgarland/typescript-vim'
-	use 'peitalin/vim-jsx-typescript'
-	use 'ThePrimeagen/vim-be-good'
-	use { 'saecki/crates.nvim',
-		tag = 'v0.3.0',
-		requires = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('crates').setup()
-		end,
-	}
-	use "fladson/vim-kitty"
+    -- Debugging
+    use 'nvim-lua/plenary.nvim'
+    use 'mfussenegger/nvim-dap'
+    use 'alx741/vim-rustfmt'
+    use 'windwp/nvim-autopairs'
+    use 'nvim-treesitter/nvim-treesitter'
+    use {
+        'windwp/nvim-ts-autotag',
+    }
+    use 'leafgarland/typescript-vim'
+    use 'peitalin/vim-jsx-typescript'
+    use 'ThePrimeagen/vim-be-good'
+    use { 'saecki/crates.nvim',
+        tag = 'v0.3.0',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('crates').setup()
+        end,
+    }
+    use "fladson/vim-kitty"
 end)
